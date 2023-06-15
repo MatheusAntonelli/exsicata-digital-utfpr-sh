c0f23bcc9d4b898a6dc3f470c55779f09244ab97
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2023-06-15 04:38:16 -0300

    Add transition to the cards, OBS: Need to fix the button state for each transitional State later.

--- src/components/Card/PlantCard.jsx
+++ src/components/Card/PlantCard.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { Card, Icon, Image, Button,TransitionablePortal,Header, Segment} from 'semantic-ui-react'
+import { Card, Icon, Image, Button,TransitionablePortal,Header, Segment,Divider} from 'semantic-ui-react'
 import '../../App.css'
 const src ='https://images.unsplash.com/photo-1623613466226-30970499b058?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=890&q=80'
 
@@ -18,45 +18,108 @@ export default class PlantCard extends Component {
     return (
       <section id='featured' className='featured'>
         <h2>Plantas coletadas</h2>
-        <Card.Group itemsPerRow={2} className='card-container'>
+        <Card.Group itemsPerRow={3} className='card-container'>
         <Card>
           <Image src='https://images.unsplash.com/photo-1623613466226-30970499b058?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=890&q=80' wrapped ui={false} />
           <Card.Content>
             <Card.Header>Nome da Planta</Card.Header>
             <Card.Meta>Data da Coleta</Card.Meta>
             <Card.Description>
-              Tipo de Planta e usos
+              Tipo de Planta e usos - POUCO TEXTO
             </Card.Description>
           </Card.Content>
           <Card.Content extra>
-            <a>
-              <Icon name='user' />
-              10 Friends
-            </a>
           </Card.Content>
           <TransitionablePortal
-        closeOnTriggerClick
-        onOpen={this.handleOpen}
-        onClose={this.handleClose}
-        openOnTriggerClick
-        trigger={
-          <Button
-            content={open ? 'Close Portal' : 'Open Portal'}
-            negative={open}
-            positive={!open}
-          />
-        }
-      >
-        <Segment
-          style={{ left: '40%', position: 'fixed', top: '50%', zIndex: 1000 }}
-        >
+            closeOnTriggerClick
+            onOpen={this.handleOpen}
+            onClose={this.handleClose}
+            openOnTriggerClick
+            trigger={
+              <Button
+                content={open ? 'Fechar Informações' : 'Mais informações'}
+                negative={open}
+                positive={!open}
+              />
+            }
+          >
+        <Segment style={{ left: '0%', position: 'fixed', top: '50%', zIndex: 1000 }} >
           <Header>Nome da planta</Header>
+          <Divider horizontal>Nome Cientifico</Divider>
+          <Header className='italic'>Nome cientifico:</Header>
           <p>Plantas medicinais podem ajudar a sociedade.</p>
-          <p>To close, simply click the close button or click away</p>
+          <p>Usos</p>
+        </Segment>
+      </TransitionablePortal>
+        </Card>
+
+        <Card>
+          <Image src='https://images.unsplash.com/photo-1623613466226-30970499b058?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=890&q=80' wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Nome da Planta</Card.Header>
+            <Card.Meta>Data da Coleta</Card.Meta>
+            <Card.Description>
+              Tipo de Planta e usos - POUCO TEXTO
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+          </Card.Content>
+          <TransitionablePortal
+            closeOnTriggerClick
+            onOpen={this.handleOpen}
+            onClose={this.handleClose}
+            openOnTriggerClick
+            trigger={
+              <Button
+                content={open ? 'Fechar Informações' : 'Mais informações'}
+                negative={open}
+                positive={!open}
+              />
+            }
+          >
+        <Segment style={{ left: '0%', position: 'fixed', top: '50%', zIndex: 1000 }} >
+          <Header>Nome da planta</Header>
+          <Divider horizontal>Nome Cientifico</Divider>
+          <Header className='italic'>Nome cientifico:</Header>
+          <p>Plantas medicinais podem ajudar a sociedade.</p>
+          <p>Usos</p>
         </Segment>
       </TransitionablePortal>
         </Card>
         
+        <Card>
+          <Image src='https://images.unsplash.com/photo-1623613466226-30970499b058?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=890&q=80' wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Nome da Planta</Card.Header>
+            <Card.Meta>Data da Coleta</Card.Meta>
+            <Card.Description>
+              Tipo de Planta e usos - POUCO TEXTO
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+          </Card.Content>
+          <TransitionablePortal
+            closeOnTriggerClick
+            onOpen={this.handleOpen}
+            onClose={this.handleClose}
+            openOnTriggerClick
+            trigger={
+              <Button
+                content={open ? 'Fechar Informações' : 'Mais informações'}
+                negative={open}
+                positive={!open}
+              />
+            }
+          >
+        <Segment style={{ left: '0%', position: 'fixed', top: '50%', zIndex: 1000 }} >
+          <Header>Nome da planta</Header>
+          <Divider horizontal>Nome Cientifico</Divider>
+          <Header className='italic'>Nome cientifico:</Header>
+          <p>Plantas medicinais podem ajudar a sociedade.</p>
+          <p>Usos</p>
+        </Segment>
+      </TransitionablePortal>
+        </Card>
 
     </Card.Group>
       </section>
