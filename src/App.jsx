6c38480bf7a99da9d103928dc2ec416360c4916a
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2024-06-11 23:21:47 -0300

    Example Test from react-three-fiber

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react'
 import MainSection from './components/MainSection/MainSection'
 import './App.css'
 import Footer from './components/Footer/Footer'
+import Hero from './components/Hero/Hero'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
        <MainSection/>
+       <Hero/>
        <Footer/>
     </>
   )
